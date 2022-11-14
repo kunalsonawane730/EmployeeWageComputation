04c6aafb64f0f03b9e8e422bc4f373abfad0dc9a
Kunal <kunalsonawane730@gmail.com>
2022-11-14 22:07:55 +0530

    adding welcom message

---
+++ src/main/java/com/bridgelabz/employeewagecomputation/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Wlcome to Employee Wage Computation" );
+    }
+}
